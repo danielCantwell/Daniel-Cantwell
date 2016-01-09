5afb2b0e51dd9a01059d2aa6ab42747fc20f57fb
Daniel Cantwell <danielCantwell@Cantwell-Apple.home>
2016-01-09 13:03:54 -0800

    Added functionality to index.js and server.js to add races

--- index.js
+++ index.js
@@ -16,129 +16,46 @@ app.config(function($routeProvider) {
         .when('/contact', {
             templateUrl: 'views/contact.html'
         })
-        .when('/resume', {
-            templateUrl: 'views/resume.html'
+        .when('/addrace', {
+            templateUrl: 'views/addrace.html'
         })
         .otherwise({
             redirectTo: '/about'
         });
 });
 
-app.controller('controller', function ($scope, $firebase, $firebaseObject, $firebaseArray, FIREBASE_URL) {
+app.controller('controller', function ($scope, $firebase, $firebaseObject, $firebaseArray, FIREBASE_URL, $window) {
 
     var ref = new Firebase(FIREBASE_URL);
     var racesRef = ref.child('races');
-
-    var r1 = {
-        name: "Coveskipper Aquathlon",
-        date: "2014-10-24",
-        overall: "34:18",
-        swim: "12:47",
-        bike: "--:--",
-        run: "21:31"
-    };
-
-    // racesRef.push(r1); 
-
-    // racesRef.on("value", function(snapshot) {
-    //     console.log(snapshot.val());
-    //     $scope.races = snapshot.val();
-    //     $scope.racecount = $scope.races.length;
-    // });
 
     $scope.races = $firebaseArray(racesRef);
     $scope.races.$loaded().then(function(races) {
         $scope.racecount = races.length;
     });
-    
+
+    $scope.addRace = function(n, d, o, s, b, r, p) {
+
+        $.post( "api/add-race", { racename: n, date: d, overall: o, swim: s, bike: b, run: r, password: p }, function(data) {
+            var result = JSON.parse(data);
+            console.log(result.status);
+
+            if (result && result.status == 200) {
+                console.log("Upload Successful")
+                $window.location.href = '/#/tri';
+            } else {
+                console.log("Upload Failed")
+                console.log(data);
+            }
+        });
+    }
 
     $scope.toggleMenu = function() {
-        // $('#menu').toggle();
-        // if $('#menu').hasClass('hidden') {
-
-        // }
         $('#menu').toggleClass('hidden');
         $('#menu').toggleClass('open');
-
-        // var isOpen = $('#menu').classList.contains('open');
-
-        // $('#menu').setAttribute('class', isOpen ? 'hidden' : 'open');
     }
 
     $('#menu').on('click', 'a', function() {
         $scope.toggleMenu();
     });
-
-    // $(document).ready(function(){
-    //     $('#racelist').on('click', 'a', function() {
-    //         console.log('click');
-    //         $(this).next('section').toggle();
-    //     });
-    // });
-
-    // var prevItem = null;
-    // $scope.toggleul = function($event) {
-    //     var e = $($event.currentTarget);
-
-    //     if (prevItem != e) {
-    //         e.next('section').hide();
-    //     } else {
-    //         e.next('section').show();
-    //     }
-
-    //     prevItem = e;
-
-    //     return false;
-    // }
-
-    // , $firebase, $firebaseObject, $firebaseArray, FIREBASE_URL
-
-    // $scope.terms = false;
-    // $scope.pledgeTaken = false;
-
-    // var ref = new Firebase(FIREBASE_URL);
-    // var pledgesRef = ref.child('pledges');
-
-    // $scope.pledges = $firebaseArray(pledgesRef);
-
-    // var updateCount = function() {
-    //     countRef = ref.child('viewCount');
-    //     countRef.once("value", function(snapshot) {
-    //         var count = snapshot.val();
-    //         count++;
-    //         countRef.set(count);
-    //     }, function(error) {
-    //         console.log("Count Read Failed");
-    //     });
-    // }
-    // updateCount();
-
-    // $scope.takePledge = function() {
-
-    //     if ($scope.pledgeForm.$invalid || !$scope.termscheck) {
-    //         console.log("inavlid");
-    //         return;
-    //     }
-
-    //     var pledgesRef = ref.child('pledges');
-
-    //     var first = $scope.firstname;
-    //     var last = $scope.lastname;
-    //     var em = $scope.email;
-    //     var zip = $scope.zip;
-
-    //     var pledge = {
-    //         firstname : first,
-    //         lastname : last,
-    //         email: em,
-    //         zip: zip
-    //     }
-
-    //     $scope.pledgeTaken = true;
-    //     pledgesRef.push(pledge);
-
-    //     $('#pledge').hide();
-    //     $('#pledgeinfoScrollButton').attr("href", "#pledgelist");
-    //     $('#mobilePledgeInfoScrollButton').attr("href", "#pledgelist");
-    // }
 });
--- server.js
+++ server.js
@@ -1,12 +1,48 @@
-var express = require('express')
-    , app = express()
-    , port = process.env.PORT || 3000
-    , router = express.Router();
+var express = require('express');
+var bodyParser = require('body-parser')
+var Firebase = require('firebase');
+var app = express();
+var port = process.env.PORT || 3000;
+var router = express.Router();
 
 app.use(express.static(__dirname + '/'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 router.get('/', function(req, res, next) {
     res.render('index.html');
+});
+
+router.post('/api/add-race', function(req, res) {
+	console.log('POST /');
+	console.dir(req.body);
+
+	if (req.body.racename && req.body.date && req.body.overall && req.body.swim && req.body.bike && req.body.run && req.body.password) {
+
+		if (req.body.password == "addrace") {
+
+			var ref = new Firebase('https://daniel-cantwell.firebaseio.com');
+		    var racesRef = ref.child('races');
+
+		    var r1 = {
+		        name: req.body.racename,
+		        date: req.body.date,
+		        overall: req.body.overall,
+		        swim: req.body.swim,
+		        bike: req.body.bike,
+		        run: req.body.run
+		    };
+
+		    racesRef.push(r1);
+		    console.log("pushed data to firebase");
+			res.end('{"success" : "Uploaded Successfully", "status" : 200}');
+		} else {
+			res.end('{"error" : "Wrong Password", "status" : 400}');
+		}
+
+	} else {
+		res.end('{"error" : "Invalid Parameters", "status" : 400}');
+	}
 });
 
 app.use('/', router);
